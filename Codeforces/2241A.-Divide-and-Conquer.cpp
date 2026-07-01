c6db88b7b979254212e4f4f43b17faf9fe452dff
Ayush Mishra <110273664+ayushm3018@users.noreply.github.com>
2026-07-01 21:59:30 +0530

    [Codeforces] 2241A. Divide and Conquer — Accepted

---
+++ Codeforces/2241A.-Divide-and-Conquer.cpp
@@ -0,0 +1,28 @@
+/*
+ * Codeforces: 2241A. Divide and Conquer
+ * URL: https://codeforces.com/problemset/problem/2241/A
+ * Language: C++20 (GCC 13-64)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while(t--) {
+        int x, y;
+        cin >> x >> y;
+        
+        if (x >= y && x % y == 0) 
+            cout << "YES" << '\n';
+         else 
+            cout << "NO" << '\n';
+        
+    }
+    return 0;
+}
