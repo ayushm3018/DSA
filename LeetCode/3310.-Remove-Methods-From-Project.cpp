e73e8eba41306d60adca7245b22af6250da696e2
Ayush Mishra <110273664+ayushm3018@users.noreply.github.com>
2026-08-06 03:29:11 +0530

    [LeetCode] 3310. Remove Methods From Project — Accepted

---
+++ LeetCode/3310.-Remove-Methods-From-Project.cpp
@@ -0,0 +1,123 @@
+/*
+ * LeetCode: 3310. Remove Methods From Project
+ * Difficulty: Medium
+ * URL: https://leetcode.com/problems/remove-methods-from-project/
+ * Language: C++
+ * Runtime: 159 ms | Memory: 301.4 MB
+ * 
+ * You are maintaining a project that has n methods numbered from 0 to n - 1.
+ * 
+ * You are given two integers n and k, and a 2D integer array invocations, where invocations[i] = [ai, bi] indicates that method ai invokes method bi.
+ * 
+ * There is a known bug in method k. Method k, along with any method invoked by it, either directly or indirectly, are considered suspicious and we aim to remove them.
+ * 
+ * A group of methods can only be removed if no method outside the group invokes any methods within it.
+ * 
+ * Return an array containing all the remaining methods after removing all the suspicious methods. You may return the answer in any order. If it is not possible to remove all the suspicious methods, none should be removed.
+ * 
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: n = 4, k = 1, invocations = [[1,2],[0,1],[3,2]]
+ * 
+ * Output: [0,1,2,3]
+ * 
+ * Explanation:
+ * 
+ * Method 2 and method 1 are suspicious, but they are directly invoked by methods 3 and 0, which are not suspicious. We return all elements without removing anything.
+ * 
+ * Example 2:
+ * 
+ * Input: n = 5, k = 0, invocations = [[1,2],[0,2],[0,1],[3,4]]
+ * 
+ * Output: [3,4]
+ * 
+ * Explanation:
+ * 
+ * Methods 0, 1, and 2 are suspicious and they are not directly invoked by any other method. We can remove them.
+ * 
+ * Example 3:
+ * 
+ * Input: n = 3, k = 2, invocations = [[1,2],[0,1],[2,0]]
+ * 
+ * Output: []
+ * 
+ * Explanation:
+ * 
+ * All methods are suspicious. We can remove them.
+ * 
+ *  
+ * 
+ * Constraints:
+ * 
+ * 	  - 1 <= n <= 10^5
+ * 
+ * 	  - 0 <= k <= n - 1
+ * 
+ * 	  - 0 <= invocations.length <= 2 * 10^5
+ * 
+ * 	  - invocations[i] == [ai, bi]
+ * 
+ * 	  - 0 <= ai, bi <= n - 1
+ * 
+ * 	  - ai != bi
+ * 
+ * 	  - invocations[i] != invocations[j]
+ */
+
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> adj(n);
+        vector<int> inDegree(n,0);
+        vector<bool> sus(n,false);
+
+        for(auto &edge : invocations){
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            inDegree[v]++;
+        }
+
+        queue<int> q;
+        q.push(k);
+        sus[k] = true;            
+
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+
+            for(int &neig : adj[curr]){
+                if(!sus[neig]){
+                    q.push(neig);
+                    sus[neig] = true;
+                }
+            }
+        }
+
+        vector<int> res;
+        bool noRemove = false;
+
+        for(auto &edge : invocations){
+            if(!sus[edge[0]] && sus[edge[1]]){
+                noRemove = true;
+                break;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(!sus[i])
+                res.push_back(i);
+        }
+
+        if(noRemove){
+            vector<int> vec(n);
+            for(int i = 0; i < n; i++)
+                vec[i] = i;
+            return vec;
+        }
+
+        return res;
+    }
+};
