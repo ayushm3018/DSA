8b97092c67ffc66268b315ce161d7bdd656c6282
Ayush Mishra <110273664+ayushm3018@users.noreply.github.com>
2026-08-17 16:13:20 +0530

    [LeetCode] 859. Buddy Strings — Accepted

---
+++ LeetCode/859.-Buddy-Strings.cpp
@@ -0,0 +1,63 @@
+/*
+ * LeetCode: 859. Buddy Strings
+ * Difficulty: Easy
+ * URL: https://leetcode.com/problems/buddy-strings/
+ * Language: C++
+ * Runtime: 0 ms | Memory: 9.3 MB
+ * 
+ * Given two strings s and goal, return true if you can swap two letters in s so the result is equal to goal, otherwise, return false.
+ * 
+ * Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at s[i] and s[j].
+ * 
+ * 	  - For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
+ * 
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: s = "ab", goal = "ba"
+ * Output: true
+ * Explanation: You can swap s[0] = 'a' and s[1] = 'b' to get "ba", which is equal to goal.
+ * 
+ * Example 2:
+ * 
+ * Input: s = "ab", goal = "ab"
+ * Output: false
+ * Explanation: The only letters you can swap are s[0] = 'a' and s[1] = 'b', which results in "ba" != goal.
+ * 
+ * Example 3:
+ * 
+ * Input: s = "aa", goal = "aa"
+ * Output: true
+ * Explanation: You can swap s[0] = 'a' and s[1] = 'a' to get "aa", which is equal to goal.
+ * 
+ *  
+ * 
+ * Constraints:
+ * 
+ * 	  - 1 <= s.length, goal.length <= 2 * 10^4
+ * 
+ * 	  - s and goal consist of lowercase letters.
+ */
+
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if(s.size()!=goal.size()) return false;
+        if(s==goal) {int freq[26]={};
+        for(char c:s){
+            freq[c-'a']++;
+            if(freq[c-'a']>=2) return true;
+        }
+        return false;
+                    }
+    
+    vector<int>diff;
+    for(int i=0; i<s.size(); i++){
+        if(s[i]!=goal[i]) diff.push_back(i);
+    }
+        if(diff.size()!=2) return false;
+        int i=diff[0], j=diff[1];
+        return s[i]==goal[j] and s[j]==goal[i];
+    }
+};
